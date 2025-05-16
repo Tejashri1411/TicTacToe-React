69a94df0f11657c4f6a7a8812015a66912787278
Tejashri <tejashridhomkar2003@gmail.com>
2025-05-16 19:41:33 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TicTacToe from './components/TicTacToe';
+// import UseStateHook from "./components/UseStateHook"
+
+function App() {
+  return (
+    <>
+      <TicTacToe />
+
+
+    </>
+  );
+}
+
+export default App;
